b77979da6050381bc087686ae3c82b276fb56bd8
moshes93 <moshesss1@gmail.com>
2018-09-20 13:45:39 +0300

    Init

--- K-Means/kernel.cu
+++ K-Means/kernel.cu
@@ -8,9 +8,9 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
+}
 
 int main()
 {
